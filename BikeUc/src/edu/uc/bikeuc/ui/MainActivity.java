e9b22dac9d9f083cab33be514f13c9c8c6d57fc2
bbachmey <bachmeb@mail.uc.edu>
2014-08-05 12:21:09 -0400

    add all button to main activity

--- BikeUc/src/edu/uc/bikeuc/ui/MainActivity.java
+++ BikeUc/src/edu/uc/bikeuc/ui/MainActivity.java
@@ -12,17 +12,28 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	//declare the buttons
+	private Button buttonObjAbout;
+	private Button buttonObjBikeRacks;
+	private Button buttonObjBikeRoutes;
+	private Button buttonObjCamera;
 	private Button buttonObjStub;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		//call the super class constructor
 		super.onCreate(savedInstanceState);
+		//grab the resource xml
 		setContentView(R.layout.activity_main);
 		
-
+		//instantiate the buttons
+		buttonObjAbout = (Button) findViewById(R.id.btnAbout);
+		buttonObjBikeRacks = (Button) findViewById(R.id.btnBikeRacks);
+		buttonObjBikeRoutes = (Button) findViewById(R.id.btnBikeRoutes);
+		buttonObjCamera = (Button) findViewById(R.id.btnCamera);
 		buttonObjStub = (Button) findViewById(R.id.btnStub);
 		
-		//attach an onclick listener to the button object
+		//attach onClick listeners to the button objects
 		buttonObjStub.setOnClickListener(new View.OnClickListener(){
 			@Override
 			public void onClick(View currentView) {
