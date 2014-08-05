c452945ccca4aa40c583da6374d4fda08c9b1154
bbachmey <bachmeb@mail.uc.edu>
2014-08-05 12:15:02 -0400

    add stub button to main activity

--- BikeUc/src/edu/uc/bikeuc/ui/MainActivity.java
+++ BikeUc/src/edu/uc/bikeuc/ui/MainActivity.java
@@ -3,15 +3,36 @@ package edu.uc.bikeuc.ui;
 import edu.uc.bikeuc.R;
 import edu.uc.bikeuc.R.layout;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	private Button buttonObjStub;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		
+
+		buttonObjStub = (Button) findViewById(R.id.btnStub);
+		
+		//attach an onclick listener to the button object
+		buttonObjStub.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View currentView) {
+				//declare the intent
+				Intent myIntent = new Intent(currentView.getContext(), StubActivity.class);
+				//act on the intent
+				startActivityForResult(myIntent,0);
+				} //end onClick
+			} // end setOnClickListener 
+		); //end new View.OnClickListener()
+		
+	} // end onCreate
 }
--- BikeUc/src/edu/uc/bikeuc/ui/StubActivity.java
+++ BikeUc/src/edu/uc/bikeuc/ui/StubActivity.java
@@ -1,15 +1,24 @@
-package edu.uc.bikeuc;
+package edu.uc.bikeuc.ui;
 
+import edu.uc.bikeuc.R;
+import edu.uc.bikeuc.R.layout;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class StubActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		//call the super class
 		super.onCreate(savedInstanceState);
+		//set the view to resource xml
 		setContentView(R.layout.activity_stub);
-	}
+		
+		
+	} //end onCreate
 }
