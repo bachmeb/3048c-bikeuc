94bc7fc0a13f3098a265ec60d9c451f6ad4d4a0b
bbachmey <bachmeb@mail.uc.edu>
2014-08-05 11:03:17 -0400

    update data transfer objects

--- BikeUc/src/edu/uc/bikeuc/dto/BikeRack.java
+++ BikeUc/src/edu/uc/bikeuc/dto/BikeRack.java
@@ -2,4 +2,43 @@ package edu.uc.bikeuc.dto;
 
 public class BikeRack {
 
+	private String rackName;  
+    private double latitude;
+    private double longitude;
+    
+    BikeRack(){
+    	//empty constructor
+    }
+
+    public BikeRack(double lat, double lon){
+    	this.latitude = lat;
+    	this.longitude = lon;
+   
+    }
+    
+    public BikeRack(double lat, double lon, String name){
+    	
+    }
+        
+    //getters and setters
+    
+	public String getRackName() {
+		return rackName;
+	}
+	public void setRackName(String rackName) {
+		this.rackName = rackName;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
 }
--- BikeUc/src/edu/uc/bikeuc/dto/BikeRoute.java
+++ BikeUc/src/edu/uc/bikeuc/dto/BikeRoute.java
@@ -1,5 +1,46 @@
 package edu.uc.bikeuc.dto;
 
-public class BikeRoute {
+import java.util.ArrayList;
 
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * @author bbachmey
+ * BikeRoute data transfer object
+ */
+public class BikeRoute {
+	
+	private int routeId;
+	private ArrayList<LatLng> latlngs;
+	
+    
+    /**
+     * 
+     */
+    public BikeRoute(){
+    	//empty constructor
+    }
+	/**
+     */
+    public BikeRoute(ArrayList<LatLng> latlngs){
+    	this.latlngs = latlngs;
+   
+    }
+
+    public int getRouteId() {
+		return routeId;
+	}
+
+	public void setRouteId(int routeId) {
+		this.routeId = routeId;
+	}
+
+	public ArrayList<LatLng> getLatlngs() {
+		return latlngs;
+	}
+
+	public void setLatlngs(ArrayList<LatLng> latlngs) {
+		this.latlngs = latlngs;
+	}
+    
 }
