acd5ec7a5f55fc8468871a1fe8eaf9c41c1fe154
bbachmey <bachmeb@mail.uc.edu>
2014-08-05 11:18:06 -0400

    update dao interfaces

--- BikeUc/src/edu/uc/bikeuc/dao/IBikeRouteDAO.java
+++ BikeUc/src/edu/uc/bikeuc/dao/IBikeRouteDAO.java
@@ -2,9 +2,22 @@ package edu.uc.bikeuc.dao;
 
 import java.util.ArrayList;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import edu.uc.bikeuc.dto.BikeRack;
 import edu.uc.bikeuc.dto.BikeRoute;
 
 public interface IBikeRouteDAO {
+	
+	public abstract void onCreate(SQLiteDatabase db);
+
+	public abstract void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion);
+	
+	public abstract void onUpdate(SQLiteDatabase db);
+
+	BikeRack populateRackFromCursor(Cursor c);
 
 	ArrayList<BikeRoute> fetchAllRoutes() throws Exception;
 
