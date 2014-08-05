1e8a229cd9e824e8b06c5d11339997b5f04e65c7
bbachmey <bachmeb@mail.uc.edu>
2014-08-05 12:01:38 -0400

    add StubActivity

---
+++ BikeUc/src/edu/uc/bikeuc/StubActivity.java
@@ -0,0 +1,15 @@
+package edu.uc.bikeuc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class StubActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_stub);
+	}
+}
